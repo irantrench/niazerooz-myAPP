7609b32c6689a7cc795e938d5d1bb5e024e37009
arian rad <arian.rad7878@gmail.com>
2025-10-25 16:21:59 +0000

    Try fixing this error: `Console Error: [2025-10-25T16:21:13.720Z] @fireb

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -2,7 +2,7 @@
 
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
+import { getFirestore, Firestore, initializeFirestore } from 'firebase/firestore';
 import { getStorage, FirebaseStorage } from 'firebase/storage';
 import { firebaseConfig } from './config';
 
@@ -16,7 +16,10 @@ if (!getApps().length) {
 }
 
 const auth: Auth = getAuth(app);
-const db: Firestore = getFirestore(app);
+const db: Firestore = initializeFirestore(app, {
+    experimentalForceLongPolling: true,
+    ignoreUndefinedProperties: true,
+});
 const storage: FirebaseStorage = getStorage(app);
 
 export { app, auth, db, storage };
